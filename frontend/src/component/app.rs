3cf6c3eb10e1695d916272d7300e99d02e945c8d
nate34k <nate34k@gmail.com>
2024-05-22 23:10:04 -0500

    Styling changes

--- frontend/src/component/app.rs
+++ frontend/src/component/app.rs
@@ -62,35 +62,39 @@ impl Component for App {
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
-            <div id="base" class="pattern">
-                <main>
-                    // Search bar
-                    <search_bar::SearchBar
-                        on_search={
-                            ctx.link().callback(|search_string| Msg::UpdateTransactionListSearch(search_string))
-                        }/>
+            <>
+                <div id="app" class="pattern">
+                </div>
+                <div class="content">
+                    <main>
+                        // Search bar
+                        <search_bar::SearchBar
+                            on_search={
+                                ctx.link().callback(|search_string| Msg::UpdateTransactionListSearch(search_string))
+                            }/>
 
-                    // Account for transaction list props
-                    <transaction_list::TransactionList should_update={
-                        {
-                            log!(format!("should_update_transaction_list in fn view(): {}", self.transaction_list_update_counter));
-                        }
-                        self.transaction_list_update_counter
-                    } search_string={
-                        {
-                            log!(format!("search_string in fn view(): {}", self.search_string));
-                        }
-                        self.search_string.clone()
-                    } update={ctx.link().callback(|should_update| Msg::UpdateProfitLoss(should_update))} />
-                    <profit_loss::ProfitLoss update_counter={
-                        {
-                            log!(format!("should_update_profit_loss in fn view(): {}", self.profit_loss_update_counter));
-                        }
-                        self.profit_loss_update_counter
-                    }/>
-                    <add_transaction_overlay::AddTransactionOverlay update={ctx.link().callback(|should_update| Msg::UpdateTransactionList(should_update))} />
-                </main>
-            </div>
+                        // Account for transaction list props
+                        <transaction_list::TransactionList should_update={
+                            {
+                                log!(format!("should_update_transaction_list in fn view(): {}", self.transaction_list_update_counter));
+                            }
+                            self.transaction_list_update_counter
+                        } search_string={
+                            {
+                                log!(format!("search_string in fn view(): {}", self.search_string));
+                            }
+                            self.search_string.clone()
+                        } update={ctx.link().callback(|should_update| Msg::UpdateProfitLoss(should_update))} />
+                        <profit_loss::ProfitLoss update_counter={
+                            {
+                                log!(format!("should_update_profit_loss in fn view(): {}", self.profit_loss_update_counter));
+                            }
+                            self.profit_loss_update_counter
+                        }/>
+                        <add_transaction_overlay::AddTransactionOverlay update={ctx.link().callback(|should_update| Msg::UpdateTransactionList(should_update))} />
+                    </main>
+                </div>
+            </>
         }
     }
 }
