1e5bba8638dd5ac8b3548c2d3e239077e4c082e4
nate34k <nate34k@gmail.com>
2024-05-30 20:40:45 -0500

    Improve search bar functionality with delayed search and timeout cancellation

--- frontend/src/component/search_bar.rs
+++ frontend/src/component/search_bar.rs
@@ -1,10 +1,9 @@
-use reqwasm::http::Request;
-use serde::{Serialize, Deserialize};
-use web_sys::HtmlInputElement;
+use web_sys::{wasm_bindgen::{closure::Closure, JsCast}, window, HtmlInputElement};
 use yew::prelude::*;
 
 pub struct SearchBar {
     item_name: String,
+    timeout_id: Option<i32>,
 }
 
 pub enum Msg {
@@ -24,14 +23,42 @@ impl Component for SearchBar {
     fn create(_ctx: &Context<Self>) -> Self {
         Self {
             item_name: String::new(),
+            timeout_id: None,
         }
     }
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
+            // Update the item name used for searching, but wait 750ms before searching after no more
             Msg::UpdateItemName(name) => {
                 self.item_name = name;
-                ctx.props().on_search.emit(self.item_name.clone());
+
+                // Cancel the current timeout
+                if let Some(timeout_id) = self.timeout_id {
+                    window()
+                        .unwrap()
+                        .clear_timeout_with_handle(timeout_id);
+                }
+
+                // Clone the context and create a callback for the timeout
+                let link = ctx.link().clone();
+                let callback = Closure::wrap(Box::new(move || {
+                    link.send_message(Msg::Search);
+                }) as Box<dyn Fn()>);
+
+                // Set a new timeout and store the timeout ID
+                let timeout_id = window()
+                    .unwrap()
+                    .set_timeout_with_callback_and_timeout_and_arguments_0(
+                        callback.as_ref().unchecked_ref(),
+                        400,
+                    )
+                    .expect("Failed to set timeout");
+
+                // Store the timeout ID and forget the closure to avoid it being dropped
+                self.timeout_id = Some(timeout_id);
+                callback.forget();
+
                 true
             },
             Msg::Search => {
